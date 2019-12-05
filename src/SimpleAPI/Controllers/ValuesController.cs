e46cb07585580a1e5c57d0deae1e5c0829cf7c6f
viru06 <virendra.mgm@gmail.com>
2019-12-05 22:36:34 +0100

    test val changed :)

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "viruaa";
+            return "viru";
         }
 
         // POST api/values
